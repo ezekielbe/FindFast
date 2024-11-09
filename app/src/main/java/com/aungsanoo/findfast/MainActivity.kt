2f98faf79b0eb9995b3d610dc380ec622cabc9c0
aungLB <aung@lionsbot.com>
2024-11-09 14:11:15 -0800

    revert fixes

--- app/src/main/java/com/aungsanoo/findfast/MainActivity.kt
+++ app/src/main/java/com/aungsanoo/findfast/MainActivity.kt
@@ -1,20 +1,44 @@
 package com.aungsanoo.findfast
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.aungsanoo.findfast.Activities.DashboardActivity
+import com.aungsanoo.findfast.Activities.LoginActivity
+import com.aungsanoo.findfast.Utils.Utils
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val isLoggedIn = handleUserSessionCheck()
+        if(!isLoggedIn)
+            handleNavigateLogin()
+    }
+
+    fun handleUserSessionCheck() : Boolean {
+        if(Utils.validUserSession()) {
+            val intent = Intent(this@MainActivity, DashboardActivity::class.java)
+            startActivity(intent)
+            finish()
+            return true;
+        }
+        return false;
+    }
+
+    fun handleNavigateLogin() {
+        val intent = Intent(this@MainActivity, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
