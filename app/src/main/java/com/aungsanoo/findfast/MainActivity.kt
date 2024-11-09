06f541b50e6deaaf56f0388cd620691b23d62b9c
ezekielbe <ezekielbeee@gmail,co>
2024-11-08 23:34:11 -0800

    LoginRegister UI

--- app/src/main/java/com/aungsanoo/findfast/MainActivity.kt
+++ app/src/main/java/com/aungsanoo/findfast/MainActivity.kt
@@ -1,47 +1,20 @@
 package com.aungsanoo.findfast
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.aungsanoo.findfast.Activities.DashboardActivity
-import com.aungsanoo.findfast.Activities.LoginActivity
-import com.aungsanoo.findfast.Utils.Utils
 
-/**
- * Activity to navigate to Login or Register or Dashboard based on User session
- */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val isLoggedIn = handleUserSessionCheck()
-        if(!isLoggedIn)
-            handleNavigateLogin()
-    }
-
-    fun handleUserSessionCheck() : Boolean {
-        if(Utils.validUserSession()) {
-            val intent = Intent(this@MainActivity, DashboardActivity::class.java)
-            startActivity(intent)
-            finish()
-            return true;
-        }
-        return false;
-    }
-
-    fun handleNavigateLogin() {
-        val intent = Intent(this@MainActivity, LoginActivity::class.java)
-        startActivity(intent)
-        finish()
     }
 }
